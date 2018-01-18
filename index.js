4ed0bbd28798647b458507a1798abb32b500ed9c
SpencerWhitehead7@gmail.com <spencerwhitehead7@gmail.com>
2018-01-18 06:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object
 }
+
+function updateObjectWithKeyAndVaue(object, key, value){
+  var newObject = object[key] = value;
+  return newObject
+}
